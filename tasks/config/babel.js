3f6d7912262e36f6b7af8aa6fa98941b8c080ffc
Rihards Fridrihsons <rfridrihsons@gmail.com>
2016-07-15 21:48:33 +0300

    Add es2015 preset to grunt babel task

--- tasks/config/babel.js
+++ tasks/config/babel.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
   grunt.config.set('babel', {
     dev: {
       options: {
-        presets: ['react']
+        presets: ['react', 'es2015']
       },
       files: [{
         expand: true,
