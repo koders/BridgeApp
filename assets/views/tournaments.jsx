defe6b4619b9493af6b10f819adebcc7a919437a
Rihards Fridrihsons <rfridrihsons@gmail.com>
2016-07-19 23:23:54 +0300

    Add language dropdown and user profile button in navbar

--- assets/views/tournaments.jsx
+++ assets/views/tournaments.jsx
@@ -21,13 +21,58 @@ $(function(){
 
     render() {
       const {language} = this.state;
-
+      var flagLanguage = language;
+      if( flagLanguage == 'en' ) {
+        flagLanguage = 'gb';
+      }
       return (
-        <p>
+        <div>
+          <ul id="dropdown1" className="dropdown-content">
+            <li><a href="#!">one</a></li>
+            <li><a href="#!">two</a></li>
+            <li className="divider"></li>
+            <li><a href="#!">three</a></li>
+          </ul>
+          <nav className="main-navigation">
+            <div className="nav-wrapper">
+              <a id="logo-container" href="#" className="brand-logo"><img src="/images/logo_web_big.jpg"/></a>
+              <ul className="nav right hide-on-med-and-down">
+                <li>
+                  <a className="profile-button waves-effect">
+                    <img src="https://z-1-scontent-fra3-1.xx.fbcdn.net/v/t1.0-9/13528749_836304686475372_3891635766179124219_n.jpg?oh=86f77c4950356ad43828429b6248de07&oe=57F0E761" alt="" className="circle" />
+                    <div className="text">
+                      <div className="title">Rihards Fridrihsons</div>
+                      <div className="subtitle">User</div>
+                    </div>
+                    <i className="fa fa-caret-down"/>
+                  </a>
+                </li>
+                <li>
+                  <div className="dropdown">
+                    <div className="dropdown-toggle language waves-effect" data-toggle="dropdown">
+                      <div className={"flag-icon flag-icon-" + flagLanguage + " flag"}></div>
+                    </div>
+                    <ul className="dropdown-menu dropdown-menu-right">
+                      <li>
+                        <a onClick={this.setLanguage.bind(this, 'lv')}>
+                          <div className="flag-icon flag-icon-lv flag"></div>
+                          <span>Latviski</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={this.setLanguage.bind(this, 'en')}>
+                          <div className="flag-icon flag-icon-gb flag"></div>
+                          <span>English</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </nav>
           {lang.get('greeting')}
-          <button className="waves-effect waves-light btn" onClick={this.setLanguage.bind(this, 'lv')}>LV</button>
-          <button className="waves-effect waves-light btn" onClick={this.setLanguage.bind(this, 'en')}>EN</button>
-        </p>
+        </div>
       );
     }
   }
