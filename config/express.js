debd7863f130eb01878ff9071178f2c979dbb3da
Rihards Fridrihsons <rfridrihsons@gmail.com>
2016-07-21 02:10:13 +0300

    Remove logs for heroku debug

--- config/express.js
+++ config/express.js
@@ -8,7 +8,6 @@ var verifyHandler = function(token, tokenSecret, profile, done) {
 
     User.findOne({uid: profile.id}, function(err, user) {
       if (user) {
-        console.log('user found:', user);
         return done(null, user);
       } else {
 
@@ -28,12 +27,7 @@ var verifyHandler = function(token, tokenSecret, profile, done) {
           data.lastname = profile.name.familyName;
         }
 
-        console.log('data', data);
-
         User.create(data, function(err, user) {
-          console.log('done creating user');
-          console.log('errors:', err);
-          console.log('user:', user);
           return done(err, user);
         });
       }
@@ -47,8 +41,6 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(uid, done) {
   User.findOne({uid: uid}, function(err, user) {
-    console.log('err in deserialization:', err);
-    console.log('user in deserialization:', user);
     done(err, user);
   });
 });
